ebd937ff7e6a442c6c5d00edbb3b293dae3d1311
test-in-dev <62110565+test-in-dev@users.noreply.github.com>
2025-07-18 12:31:45 +0500

    Add .next/static/chunks/src_app_library_page_tsx_6b5fd1c9._.js 1752823905091

---
+++ .next/static/chunks/src_app_library_page_tsx_6b5fd1c9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ecdacd31._.js",
+  "static/chunks/node_modules_a1b1f4e0._.js"
+],
+    source: "dynamic"
+});
