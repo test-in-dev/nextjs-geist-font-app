99ec688d065e2206a253800c6c4980cd5bd3522f
test-in-dev <62110565+test-in-dev@users.noreply.github.com>
2025-07-18 12:32:07 +0500

    Add .next/static/chunks/src_app_attendance_page_tsx_6b5fd1c9._.js 1752823926062

---
+++ .next/static/chunks/src_app_attendance_page_tsx_6b5fd1c9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_95079638._.js",
+  "static/chunks/node_modules_69e904c4._.js"
+],
+    source: "dynamic"
+});
