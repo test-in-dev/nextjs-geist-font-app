124e5a6b920661756b2ddbec924da85be60c6ddc
test-in-dev <62110565+test-in-dev@users.noreply.github.com>
2025-07-18 12:32:03 +0500

    Add .next/static/chunks/src_app_exams_page_tsx_6b5fd1c9._.js 1752823922917

---
+++ .next/static/chunks/src_app_exams_page_tsx_6b5fd1c9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4af649cb._.js",
+  "static/chunks/node_modules_5d2f546f._.js"
+],
+    source: "dynamic"
+});
